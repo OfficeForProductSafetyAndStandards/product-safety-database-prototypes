4043879ce1949a71cc0e9c162c805a5ba4d1dc5b
Darren Harridence <DH02@users.noreply.github.com>
2021-05-04 20:01:53 +0100

    more flow pages

--- app/routes.js
+++ app/routes.js
@@ -72,7 +72,8 @@ router.post('/add-product-v4/do-you-have-the-barcode', function (req, res) {
 })
 router.post('/add-product-v4/do-you-have-the-barcode-error', function (req, res) {
   var barcode1 = req.session.data['barcode']
-  if ((barcode == 'yes') && (barnumber != '')) {
+  var barnumber1 = req.session.data['barnumber']
+  if ((barcode1 == 'yes') && (barnumber1 != '')) {
     res.redirect('/add-product-v4/select-a-product-barCodeTxt')
   } else if (barcode1 == 'no') {
     return res.redirect('/add-product-v4/search-for-an-existing-product')
@@ -113,11 +114,11 @@ router.post('/add-product-v4/search-for-an-existing-product-error', function (re
 //************* start select-a-product
 router.post('/add-product-v4/select-a-product', function (req, res) {
   var productitem = req.session.data['product-item']
-//
+
   if (productitem == null) {
     return res.redirect('/add-product-v4/select-a-product-error')
   } else {
-    return res.redirect('/add-product-v4/what-is-the-product-name')
+    return res.redirect('/add-product-v4/does-the-product-have-a-brand')
   }
 })
 router.post('/add-product-v4/select-a-product-error', function (req, res) {
@@ -126,13 +127,115 @@ router.post('/add-product-v4/select-a-product-error', function (req, res) {
   if (productitem2 == null) {
     return res.redirect('/add-product-v4/select-a-product-error')
   } else {
+    return res.redirect('/add-product-v4/does-the-product-have-a-brand')
+  }
+})
+//*************************************************** ends
+
+//************* start does-the-product-have-a-brand
+router.post('/add-product-v4/does-the-product-have-a-brand', function (req, res) {
+  var branded = req.session.data['branded']
+  var brand = req.session.data['brand']
+
+  if (branded == null) {
+    return res.redirect('/add-product-v4/does-the-product-have-a-brand-error')
+  } else if ((branded == 'yes') && (brand == '')) {
+    return res.redirect('/add-product-v4/does-the-product-have-a-brand-error-2')
+  } else {
+    return res.redirect('/add-product-v4/what-is-the-product-name')
+  }
+})
+router.post('/add-product-v4/does-the-product-have-a-brand-error', function (req, res) {
+  var branded1 = req.session.data['branded']
+  var brand1 = req.session.data['brand']
+
+  if (branded1 == null) {
+    return res.redirect('/add-product-v4/does-the-product-have-a-brand-error')
+  } else if ((branded1 == 'yes') && (brand1 == '')) {
+    return res.redirect('/add-product-v4/does-the-product-have-a-brand-error-2')
+  } else if ((branded1 == 'yes') && (brand1 != '')) {
+    return res.redirect('/add-product-v4/what-is-the-product-name')
+  }
+})
+router.post('/add-product-v4/does-the-product-have-a-brand-error-2', function (req, res) {
+  var branded2 = req.session.data['branded']
+  var brand2 = req.session.data['brand']
+
+  if (branded2 == null) {
+    return res.redirect('/add-product-v4/does-the-product-have-a-brand-error')
+  } else if ((branded2 == 'yes') && (brand2 == '')) {
+    return res.redirect('/add-product-v4/does-the-product-have-a-brand-error-2')
+  } else if ((branded2 == 'yes') && (brand2 != '')) {
     return res.redirect('/add-product-v4/what-is-the-product-name')
   }
 })
 //*************************************************** ends
 
-//} else if ((barcode == 'yes') && (barnumber == '2021')) {
-    //return res.redirect('/add-product-v4/select-a-product-barCodeTxt')
+//************* start search for existing product
+router.post('/add-product-v4/what-is-the-product-name', function (req, res) {
+  var productname = req.session.data['productname']
+
+  if (productname == '') {
+    return res.redirect('/add-product-v4/what-is-the-product-name-error')
+  } else {
+    return res.redirect('/add-product-v4/what-is-the-product-category')
+  }
+})
+router.post('/add-product-v4/what-is-the-product-name-error', function (req, res) {
+  var productname2 = req.session.data['productname']
+
+  if (productname2 == '') {
+    return res.redirect('/add-product-v4/what-is-the-product-name-error')
+  } else {
+    return res.redirect('/add-product-v4/what-is-the-product-category')
+  }
+})
+//*************************************************** ends
+
+//************* start what-is-the-product-category
+router.post('/add-product-v4/what-is-the-product-category', function (req, res) {
+  var category = req.session.data['category']
+  var subcategory = req.session.data['subcategory']
+
+  if ((category == '') && (subcategory == '')) {
+    return res.redirect('/add-product-v4/what-is-the-product-category-error-2')
+  } else if ((category == '') && (subcategory != '')) {
+    return res.redirect('/add-product-v4/what-is-the-product-category-error')
+  } else if ((category != '') && (subcategory == '')) {
+    return res.redirect('/add-product-v4/what-is-the-product-category-error-3')
+  } else {
+    return res.redirect('/add-product-v4/does-the-product-have-marking')
+  }
+})
+router.post('/add-product-v4/what-is-the-product-category-error', function (req, res) {
+  var category1 = req.session.data['category']
+  var subcategory1 = req.session.data['subcategory']
+
+  if ((category1 == '') && (subcategory1 == '')) {
+    return res.redirect('/add-product-v4/what-is-the-product-category-error-2')
+  } else if ((category1 == '') && (subcategory1 != '')) {
+    return res.redirect('/add-product-v4/what-is-the-product-category-error')
+  } else if ((category1 != '') && (subcategory1 == '')) {
+    return res.redirect('/add-product-v4/what-is-the-product-category-error-3')
+  } else {
+    return res.redirect('/add-product-v4/does-the-product-have-marking')
+  }
+})
+router.post('/add-product-v4/what-is-the-product-category-error-3', function (req, res) {
+  var category3 = req.session.data['category']
+  var subcategory3 = req.session.data['subcategory']
+
+  if ((category3 == '') && (subcategory3 == '')) {
+    return res.redirect('/add-product-v4/what-is-the-product-category-error-2')
+  } else if ((category3 == '') && (subcategory3 != '')) {
+    return res.redirect('/add-product-v4/what-is-the-product-category-error')
+  } else if ((category3 != '') && (subcategory3 == '')) {
+    return res.redirect('/add-product-v4/what-is-the-product-category-error-3')
+  } else {
+    return res.redirect('/add-product-v4/does-the-product-have-marking')
+  }
+})
+//*************************************************** ends
 
 
 //************* routes for add-product-v4 ************* ends
