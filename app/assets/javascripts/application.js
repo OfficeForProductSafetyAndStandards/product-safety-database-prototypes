0af1bb4cbc5ed15708ae5fd2d41eee86f9480c24
empathicdesign <empathybydesign@gmail.com>
2023-10-06 02:06:05 +0100

    PRISM and image updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,41 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+ $(document).ready(function () {
+            // Bind a change event to the file input
+            $('#imageUpload').change(function () {
+                if ($(this).val()) {
+                    // File is selected, hide singleUpload and show uploadAnother
+                    $('#singleUpload').hide();
+                    $('#imageUpload').hide();
+                    $('#uploadAnother').show();
+                    $('#fileDisplay').show();
+                } else {
+                    // No file selected, show singleUpload and hide uploadAnother
+                    $('#singleUpload').show();
+                    $('#imageUpload').show();
+                    $('#uploadAnother').hide();
+                    $('#fileDisplay').hide();
+                }
+            });
+        });
+
+  $(document).ready(function () {
+            // Click event handler for the uploadAnotherbtn
+            $('#uploadAnotherbtn').click(function (e) {
+                e.preventDefault(); // Prevent the default link behavior
+
+                // Toggle the visibility of the divs
+                $('#singleUpload').show();
+                $('#uploadAnother').hide();
+                $('#imageUpload').toggle();
+
+                // Show "No file chosen" text in the input
+                if ($('#imageUpload').is(':visible')) {
+                    $('#imageUpload').val(''); // Clear the file input
+                    $('#imageUpload').attr('value', ''); // Clear the file input's value attribute
+                    $('#imageUpload').next('.custom-file-label').text('No file chosen');
+                }
+            });
+        });
