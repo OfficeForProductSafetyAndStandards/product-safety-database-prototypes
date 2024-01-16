55832c3d45ed489089725624697f5e8e33e2dc0b
stuartogier <stuartogier@gmail.com>
2024-01-16 08:45:41 +0000

    Duplicate records

--- app/views/psd-revised-flow/add-business-to-notification/_routes.js
+++ app/views/psd-revised-flow/add-business-to-notification/_routes.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 
-
 // Search or add a business //
 router.post('/search-businesses', function (req, res) {
   res.redirect('./how-to-add-business'); 
@@ -10,7 +9,6 @@ router.post('/search-businesses', function (req, res) {
 // How to add a business //
 router.post('/how-to-add-business', function (req, res) {
   const addBusiness = req.session.data['add-business'];
-
   if (addBusiness === 'companies-house') {
     res.redirect('./search-companies-house');
   } else if (addBusiness === 'manually') {
@@ -20,12 +18,10 @@ router.post('/how-to-add-business', function (req, res) {
   }
 });
 
-
 // Manual //
 router.post('/enter-business-details', function (req, res) {
   res.redirect('./business-blank'); 
 })
-
 
 // Bartholmew //
 router.post('/business-bartholomew', function (req, res) {
@@ -37,6 +33,11 @@ router.post('/business-cunningham', function (req, res) {
   res.redirect('./business-role'); 
 })
 
+// Dreams //
+router.post('/business-dreams', function (req, res) {
+  res.redirect('./business-role'); 
+})
+
 // Blank //
 router.post('/business-blank', function (req, res) {
   res.redirect('./business-role'); 
@@ -44,7 +45,7 @@ router.post('/business-blank', function (req, res) {
 
 // Edit details //
 router.post('/edit-business-details', function (req, res) {
-  res.redirect('./business-blank'); 
+  res.redirect('./business-dreams'); 
 })
 
 // Business role //
@@ -52,11 +53,14 @@ router.post('/business-role', function (req, res) {
   res.redirect('./added-business-summary'); 
 })
 
+// Duplicate  //
+router.post('/duplicate-business', function (req, res) {
+  res.redirect('./business-dreams'); 
+})
 
 // Business summary //
 router.post('/added-business-summary', function (req, res) {
   const changedName = req.session.data['changedName'];
-
   if (changedName === 'yes') {
     res.redirect('./search-businesses');
   } else if (changedName === 'no') {
@@ -65,7 +69,5 @@ router.post('/added-business-summary', function (req, res) {
     res.status(204).end(); // <-- No redirect when not selected
   }
 });
-
-
 
 module.exports = router
