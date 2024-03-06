3f329eacc45756773a7031c7e325fa4ec1848230
stuartogier <stuartogier@gmail.com>
2024-03-06 08:59:36 +0000

    Homepage update

--- app/views/psd-revised-flow/search/_routes.js
+++ app/views/psd-revised-flow/search/_routes.js
@@ -1,22 +1,16 @@
 const express = require('express')
 const router = express.Router()
 
-// Search or add a business //
 router.post('/your-name', function (req, res) {
   res.redirect('./your-account'); 
 })
 
-// Search or add a business //
 router.post('/your-account', function (req, res) {
   res.redirect('./your-name'); 
 })
 
-// Search or add a business //
 router.post('/notifications', function (req, res) {
   res.redirect('./task-list'); 
 })
-
-
-
 
 module.exports = router
